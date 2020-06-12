52e102beb15936845e91274bbb1788ab649a4c58
viniciussaturnino <viniciussaturnino07@hotmail.com>
2020-06-12 19:02:36 -0300

    Some tests with Insomnia

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
     return response.send('Hello World');
 });
 
+app.post('/users', (request, response) => {
+    const user = request.body;
+
+    return response.json(user);
+});
+
 app.get('/users', (request, response) => {
-    return response.json([
-        'Matola',
-        'Maia',
-        'Monteiro',
-        'Alonso'
-    ]);
+    const data = request.query;
+    
+    return response.json(data);
 });
 
 app.listen(3333);
