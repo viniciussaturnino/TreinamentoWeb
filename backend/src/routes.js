6b065fe5d2c4c60cc9fd308021340073cf163059
viniciussaturnino <viniciussaturnino07@hotmail.com>
2020-06-13 16:34:25 -0300

    Created GET method for listing de airlines

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,9 +2,19 @@ const express = require('express');
 const crypto = require('crypto');
 
 const connection = require('./database/connection');
+const { request } = require('http');
+const { response } = require('express');
 
 const routes = express.Router();
 
+// Listing route for all airlines
+routes.get('/airlines', async (request, response) => {
+    const airlines = await connection('airlines').select('*');
+
+    return response.json(airlines);
+});
+
+// Creation route for a new airline
 routes.post('/airlines', async (request, response) => {
     const {name, email, city, uf} = request.body;
     // create a random ID
@@ -20,5 +30,22 @@ routes.post('/airlines', async (request, response) => {
 
     return response.json({ id });
 });
+
+
+/*
+routes.post('/flights', async (request, response) => {
+    const {destiny, date, hour, value} = request.body;
+
+    await connection('flights').insert({
+        id,
+        destiny,
+        date,
+        hour,
+        value
+    })
+
+    return response.json({ id });
+});*/
+
 
 module.exports = routes;
