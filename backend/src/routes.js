1f0b72548803dd494cda986dd57e39a22ccbe134
viniciussaturnino <viniciussaturnino07@hotmail.com>
2020-06-13 18:37:34 -0300

    Created a method for listing specifics flights

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const AirlineController = require('./controllers/AirlineController');
 const FlightController = require('./controllers/FlightController');
+const ProfileController = require('./controllers/ProfileController');
 
 const routes = express.Router();
 
 routes.get('/airlines', AirlineController.index);
 routes.post('/airlines', AirlineController.create);
+
+routes.get('/profile', ProfileController.index);
 
 routes.get('/flights', FlightController.index);
 routes.post('/flights', FlightController.create);
