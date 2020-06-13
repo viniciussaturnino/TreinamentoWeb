d2e42685cfca735a44286067114d5d88fae9b39b
viniciussaturnino <viniciussaturnino07@hotmail.com>
2020-06-13 19:07:25 -0300

    Created a login method

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,8 +2,11 @@ const express = require('express');
 const AirlineController = require('./controllers/AirlineController');
 const FlightController = require('./controllers/FlightController');
 const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
 
 routes.get('/airlines', AirlineController.index);
 routes.post('/airlines', AirlineController.create);
