363e3a0c46f7d64326b984e0259f0e8510ae3ec0
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2020-06-13 21:20:41 -0300

    Created Login and Register components, and created Routes with react-router-dom

---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+export default function Login() {
+    return (
+        <div className="login-container">
+            <section className="form">
+                
+            </section>
+        </div>
+    );
+}
