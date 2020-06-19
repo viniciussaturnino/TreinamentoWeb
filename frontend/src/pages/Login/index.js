bb150ba616517cac3ac6a6327858484ce2d0afa1
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2020-06-18 23:37:52 -0300

    Created js for login page

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { FiUserPlus } from 'react-icons/fi';
 import './styles.css';
+
+import logo from '../../assets/logo.svg';
+import plane from '../../assets/big-plane.svg'
 
 export default function Login() {
     return (
         <div className="login-container">
             <section className="form">
+                <img src={logo} alt="FGAirlines" />
                 
+                <form>
+                    <h1>Faça seu login</h1>
+
+                    <input placeholder="Sua ID" />
+                    <button type="submit">Entrar</button>
+
+                    <a href="/register">
+                        <FiUserPlus size={16} color="#17333C" />
+                        Não tem cadastro? Registre-se
+                    </a>
+                </form>
             </section>
+
+            <img src={plane} alt="Airplane" />
         </div>
     );
 }
