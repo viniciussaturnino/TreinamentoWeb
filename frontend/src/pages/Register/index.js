c47920328c56209fcf804de40493bd5bd736a2a8
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2020-06-23 01:12:21 -0300

    Added cors to backend and made register connection between front and backend

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
+import api from '../../services/api';
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
 
 export default function Register() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [city, setCity] = useState('');
+    const [uf, setUf] = useState('');
+
+    async function handleRegister(e) {
+        e.preventDefault();
+
+        const data = {
+            name,
+            email,
+            city,
+            uf,
+        };
+
+        try{
+            const response = await api.post('airlines',data);
+
+            alert(`Seu ID de acesso: ${response.data.id}`);
+        } catch(err){
+            alert('Erro no cadastro, tente novamente.');
+        }
+    }
+
     return (
         <div className="register-container">
             <div className="content">
@@ -22,12 +47,28 @@ export default function Register() {
                     </Link>
                 </section>
 
-                <form>
-                    <input placeholder="Nome da linha aérea" />
-                    <input type="email" placeholder="E-mail" />
-                    <input placeholder="Cidade" />
+                <form onSubmit={handleRegister}>
+                    <input 
+                        placeholder="Nome da linha aérea"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    />
+                    <input
+                        type="email" placeholder="E-mail"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <input
+                        placeholder="Cidade" 
+                        value={city}
+                        onChange={e => setCity(e.target.value)}
+                    />
                     <div className="form-group">
-                        <input placeholder="UF" style={{ width: 80 }} />
+                        <input
+                            placeholder="UF" style={{ width: 80 }}
+                            value={uf}
+                            onChange={e => setUf(e.target.value)}
+                        />
                         <button type="submit">Cadastrar</button>
                     </div>
                     
