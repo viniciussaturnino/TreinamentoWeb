f364391ed134a7098e0cd806cdaded8ba554aaea
viniciussaturnino <viniciussaturnino07@hotmail.com>
2020-06-23 19:20:26 -0300

    Connected register page with backend

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -13,8 +13,11 @@ export default function Register() {
     const [city, setCity] = useState('');
     const [uf, setUf] = useState('');
 
+    const history = useHistory();
+
     async function handleRegister(e) {
         e.preventDefault();
+
 
         const data = {
             name,
@@ -25,8 +28,8 @@ export default function Register() {
 
         try{
             const response = await api.post('airlines',data);
-
             alert(`Seu ID de acesso: ${response.data.id}`);
+            history.push('/');
         } catch(err){
             alert('Erro no cadastro, tente novamente.');
         }
