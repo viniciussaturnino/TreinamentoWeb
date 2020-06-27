4ef441507482419e7a1b3a29efe37d242b8dec30
viniciussaturnino <viniciussaturnino07@hotmail.com>
2020-06-26 21:02:38 -0300

    Fixed some errors with GET method for listing flgihts

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -6,8 +6,8 @@ module.exports = {
         const { email, password } = request.body;
 
         // const airline = await connection('airlines').where('id', id).select('name').first();
-        const airline_email = await connection('airlines').where('email', email).select('name').first();
-        const airline_password = await connection('airlines').where('password', password).select('name').first();
+        const airline_email = await connection('airlines').where('email', email).select('*').first();
+        const airline_password = await connection('airlines').where('password', password).select('*').first();
 
         if(!airline_email || !airline_password){
             return response.status(400).json({ error: 'Email ou senha não correspondem' });
