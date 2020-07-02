b074fe1dc233818d60ed4de8137b91a0d8c52243
viniciussaturnino <viniciussaturnino07@hotmail.com>
2020-07-02 20:26:24 -0300

    Clean the code

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -2,18 +2,16 @@ const connection = require('../database/connection');
 
 module.exports = {
     async create(request, response){
-        // const { id } = request.body;
         const { email, password } = request.body;
 
         const airline = await connection('airlines').where('email', email).select('*').first();
-        //const airline_password = await connection('airlines').where('password', password).select('*').first();
+        
         if(!airline)
             return response.status(404).send({ error: 'Usuário inexistente' });
-        // console.log(user.password);
-        if(airline.password !== password){
+        
+        if(airline.password !== password)
             return response.status(400).json({ error: 'Senha inválida' });
-        }
-
+        
         return response.json(airline);
     }
 }
