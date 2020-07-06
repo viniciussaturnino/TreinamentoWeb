b9e75c3b88954775664e2d91621151c34f56c10a
viniciussaturnino <viniciussaturnino07@hotmail.com>
2020-07-05 22:55:51 -0300

    Created forgot password page style

--- frontend/src/pages/ForgotPassword/index.js
+++ frontend/src/pages/ForgotPassword/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import logo from '../../assets/logo.svg';
+
 import './styles.css';
 
 export default function ForgotPasword(){
-    return(
-        <div>
-            <h1>Página de esqueci minha senha</h1>
+    return (
+        <div className="forgot-container">
+            <div className="content">
+                <section>
+                    <img src={logo} alt="FGAirlines" />
+
+                    <h1>Esqueceu sua senha?</h1>
+                    <p>Preencha com o seu email para receber um link para atualizar sua senha</p>
+
+                    <Link className="back-link" to="/">
+                        <FiArrowLeft size={16} color="#17333C" />
+                        Voltar
+                    </Link>
+                </section>
+
+                <form>
+                    <input type="email" placeholder="E-mail"/>
+                    <button type="submit">Enviar email</button>
+                </form>
+            </div>
         </div>
     );
 }
