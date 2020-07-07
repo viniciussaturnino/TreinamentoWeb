6159bdf50549122ad807143fc25bac9e2acab35a
viniciussaturnino <viniciussaturnino07@hotmail.com>
2020-07-06 21:47:08 -0300

    Created recover password page style

--- frontend/src/pages/RecoverPassword/index.js
+++ frontend/src/pages/RecoverPassword/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
 import './styles.css';
 
 export default function RecoverPasword(){
     return(
-        <div>
-            <h1>Página de recuperação de senha</h1>
+        <div class="form-structor">
+            <div class="recover">
+                <h1 class="form-title">Recuperar senha</h1>
+                <div class="form-holder">
+                    <form>
+                        <input type="password" placeholder="Nova Senha" />
+                        <input type="password" placeholder="Confirme a Senha" />
+                    </form>
+                </div>
+                <button class="button">Atualizar senha</button>
+            </div>
+            <Link className="back-link" to="/">
+                <FiArrowLeft size={16} color="#17333C" />
+                Voltar
+            </Link>
         </div>
     );
 }
